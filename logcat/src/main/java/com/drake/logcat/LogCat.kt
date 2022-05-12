97db8284a2aed1997ecf7444b326befa8a6efcce
drake <liangjinggpx@gmail.com>
2022-05-12 20:51:46 +0800

    修改日志msg类型为Any

--- logcat/src/main/java/com/drake/logcat/LogCat.kt
+++ logcat/src/main/java/com/drake/logcat/LogCat.kt
@@ -83,7 +83,7 @@ object LogCat {
     @JvmOverloads
     @JvmStatic
     fun v(
-        msg: String?,
+        msg: Any?,
         tag: String = this.tag,
         tr: Throwable? = null,
         occurred: Throwable? = Exception()
@@ -95,7 +95,7 @@ object LogCat {
     @JvmOverloads
     @JvmStatic
     fun i(
-        msg: String?,
+        msg: Any?,
         tag: String = this.tag,
         tr: Throwable? = null,
         occurred: Throwable? = Exception()
@@ -106,7 +106,7 @@ object LogCat {
     @JvmOverloads
     @JvmStatic
     fun d(
-        msg: String?,
+        msg: Any?,
         tag: String = this.tag,
         tr: Throwable? = null,
         occurred: Throwable? = Exception()
@@ -117,7 +117,7 @@ object LogCat {
     @JvmOverloads
     @JvmStatic
     fun w(
-        msg: String?,
+        msg: Any?,
         tag: String = this.tag,
         tr: Throwable? = null,
         occurred: Throwable? = Exception()
@@ -128,7 +128,7 @@ object LogCat {
     @JvmOverloads
     @JvmStatic
     fun e(
-        msg: String?,
+        msg: Any?,
         tag: String = this.tag,
         tr: Throwable? = null,
         occurred: Throwable? = Exception()
@@ -139,7 +139,7 @@ object LogCat {
     @JvmOverloads
     @JvmStatic
     fun wtf(
-        msg: String?,
+        msg: Any?,
         tag: String = this.tag,
         tr: Throwable? = null,
         occurred: Throwable? = Exception()
@@ -158,87 +158,88 @@ object LogCat {
      */
     private fun print(
         type: Type = INFO,
-        msg: String? = null,
+        msg: Any? = null,
         tag: String = this.tag,
         tr: Throwable? = null,
         occurred: Throwable? = Exception()
     ) {
         if (!enabled || msg == null) return
 
-        val info = LogInfo(type, msg, tag, tr, occurred)
+        var message = msg.toString()
+
+        val info = LogInfo(type, message, tag, tr, occurred)
         for (logHook in logHooks) {
             logHook.hook(info)
             if (info.msg == null) return
         }
 
-        var adjustMsg = msg
         if (traceEnabled && occurred != null) {
             occurred.stackTrace.getOrNull(1)?.run {
-                adjustMsg += " ($fileName:$lineNumber)"
+                message += " ($fileName:$lineNumber)"
             }
         }
         val max = 3800
-        val length = adjustMsg.length
+        val length = message.length
         if (length > max) {
             synchronized(this) {
                 var startIndex = 0
                 var endIndex = max
                 while (startIndex < length) {
                     endIndex = min(length, endIndex)
-                    val substring = adjustMsg.substring(startIndex, endIndex)
+                    val substring = message.substring(startIndex, endIndex)
                     log(type, substring, tag, tr)
                     startIndex += max
                     endIndex += max
                 }
             }
         } else {
-            log(type, adjustMsg, tag, tr)
+            log(type, message, tag, tr)
         }
     }
 
     /**
-     * Json格式输出Log
-     *
-     * @param msg JSON
-     * @param tag     标签
-     * @param url     地址
+     * Json格式化输出日志
+     * @param tag 日志标签
+     * @param msg 日志信息
+     * @param type 日志类型
+     * @param occurred 日志发生位置
      */
     @JvmOverloads
     @JvmStatic
     fun json(
-        msg: String?,
+        json: Any?,
         tag: String = this.tag,
-        url: String? = null,
+        msg: String = "",
         type: Type = INFO,
         occurred: Throwable? = Exception()
     ) {
-        if (!enabled || msg == null) return
+        if (!enabled || json == null) return
 
-        if (msg.isNullOrBlank()) {
-            val adjustMsg = if (url.isNullOrBlank()) msg else url
-            print(type, adjustMsg, tag)
+        var message = json.toString()
+
+        val occurredMsg = if (traceEnabled && occurred != null) {
+            occurred.stackTrace.getOrNull(1)?.run { " ($fileName:$lineNumber)" }
+        } else ""
+
+        if (message.isBlank()) {
+            print(type, "$msg$occurredMsg\n$message", tag, occurred = null)
             return
         }
 
-        val tokener = JSONTokener(msg)
+        val tokener = JSONTokener(message)
         val obj = try {
             tokener.nextValue()
         } catch (e: Exception) {
             "Parse json error"
         }
 
-        var adjustMsg = when (obj) {
-            is JSONObject -> {
-                obj.toString(2)
-            }
-            is JSONArray -> {
-                obj.toString(2)
-            }
+        message = when (obj) {
+            is JSONObject -> obj.toString(2)
+            is JSONArray -> obj.toString(2)
             else -> obj.toString()
         }
-        if (!url.isNullOrBlank()) adjustMsg = "$url\n$adjustMsg"
 
-        print(type, adjustMsg, tag, occurred)
+        print(type, "$msg$occurredMsg\n$message", tag, occurred = null)
     }
 
     private fun log(type: Type, msg: String, tag: String, tr: Throwable?) {
